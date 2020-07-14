47b257d3aa578140bb6a9cd8a12ee43c2563f827
hunter <wenze@sinovatech.com>
2020-07-14 10:52:36 +0800

    DefaultTabController tabbar creat listview defaultdata write

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,7 +1,12 @@
 //import 'dart:html';
 
 import 'package:flutter/material.dart';
+// 一些设备判断
+import 'dart:io';
 
+// 设置一些全局变量
+// 透明色
+int clearColor = 0x00FFFFFF;
 //import 'package:english_words/english_words.dart';
 
 // =>  flutter单行函数或者方法的简写
@@ -106,90 +111,134 @@ class myFirstPage extends StatelessWidget {
   @override
   // 每个类中都有一个bulid
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('首页'),
-        // 是否居中 ios中默认居中 安卓在左边
-        centerTitle: true,
-        // 颜色
-        backgroundColor: Colors.red,
-        // 字体颜色和样式
-        textTheme: TextTheme(),
-        // 右边放一个按钮
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                // 写个弹框
-                showDialog(
-                    context: context,
-                    barrierDismissible: false,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text('提示'),
-                        content: Text('内容详情'),
-                        actions: [
-                          FlatButton(
-                              onPressed: () {
-                                //NavigatorUtils.goBack(context);
-                                Navigator.of(context).pop();
-                                //Navigator.pop(context);
-                              },
-                              child: Text('取消')),
-                          FlatButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                                // Navigator.pop(context)
-                              },
-                              child: Text('确定'))
-                        ],
-                      );
-                    });
-              })
-        ],
-      ),
-      // Drawer侧滑效果
-      drawer: Drawer(
-        child: ListView(
-          // 头部有空隙的解决办法
-          padding: EdgeInsets.all(0),
-          children: [
-            // 头部
-            UserAccountsDrawerHeader(
-              accountName: Text(
-                '张三',
-                style: TextStyle(
-                  // 字体颜色
-                  color: Colors.red,
-                ),
-                // 设置字体段落
-                strutStyle: StrutStyle(
-                  // 字体大小
-                  fontSize: 40,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              accountEmail: Text('abc@qq.com'),
-              currentAccountPicture: CircleAvatar(
-                // 颜色设置为透明
-                backgroundColor: Color(0x00FFFFFF),
-                // 网页图片
-                backgroundImage: NetworkImage(
-                    'http://gx.10086.cn/zt-portal/gxhzg/nav/upload//pageDesign/s2Ip1Rbvdfm30xeZMDVe.png'),
-                // backgroundImage:
-                //     Image.asset('images/z1502640178971_350771.jpg'),
-              ),
-              decoration: BoxDecoration(
-                // 让图片拉升 布满整个屏幕
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  // 这里可以加载网页图片 NetworkImage
-                  image: AssetImage('images/z1502640178971_350771.jpg'),
-                ),
-              ),
-            ),
+    //return Scaffold(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('首页'),
+          // 是否居中 ios中默认居中 安卓在左边
+          centerTitle: true,
+          // 颜色
+          backgroundColor: Colors.red,
+          // 字体颜色和样式
+          textTheme: TextTheme(),
+          // 右边放一个按钮
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  // 写个弹框
+                  showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('提示'),
+                          content: Text('内容详情'),
+                          actions: [
+                            FlatButton(
+                                onPressed: () {
+                                  //NavigatorUtils.goBack(context);
+                                  Navigator.of(context).pop();
+                                  //Navigator.pop(context);
+                                },
+                                child: Text('取消')),
+                            FlatButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  // Navigator.pop(context)
+                                },
+                                child: Text('确定'))
+                          ],
+                        );
+                      });
+                })
           ],
         ),
+        // Drawer侧滑效果
+        drawer: Drawer(
+          child: ListView(
+            // 头部有空隙的解决办法
+            padding: EdgeInsets.all(0),
+            children: [
+              // 头部
+              UserAccountsDrawerHeader(
+                accountName: Text(
+                  '张三',
+                  style: TextStyle(
+                    // 字体颜色
+                    color: Colors.red,
+                  ),
+                  // 设置字体段落
+                  strutStyle: StrutStyle(
+                    // 字体大小
+                    fontSize: 40,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                accountEmail: Text('abc@qq.com'),
+                currentAccountPicture: CircleAvatar(
+                  // 颜色设置为透明
+                  backgroundColor: Color(0x00FFFFFF),
+                  // 网页图片
+                  backgroundImage: NetworkImage(
+                      'http://gx.10086.cn/zt-portal/gxhzg/nav/upload//pageDesign/s2Ip1Rbvdfm30xeZMDVe.png'),
+                  // backgroundImage:
+                  //     Image.asset('images/z1502640178971_350771.jpg'),
+                ),
+                decoration: BoxDecoration(
+                  // 让图片拉升 布满整个屏幕
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    // 这里可以加载网页图片 NetworkImage
+                    image: AssetImage('images/z1502640178971_350771.jpg'),
+                  ),
+                ),
+              ),
+              ListTile(title: Text('设置'), trailing: Icon(Icons.settings)),
+              // 加横线
+              Divider(color: Colors.red),
+              ListTile(
+                title: Text('我要收藏'),
+                subtitle: Text('收藏,可以有更多惊喜'),
+                trailing: Icon(Icons.collections),
+              ),
+              Divider(color: Colors.blue),
+              ListTile(
+                title: Text('注销'),
+                trailing: Icon(Icons.exit_to_app),
+                onTap: () {
+                  print('打开注销界面');
+                },
+              ),
+            ],
+          ),
+        ),
+        // 给bottomNavigationBar加背景色 TabBar 有个指示器 隐藏 设置成50 在布局下tabbar :labelStyle
+        bottomNavigationBar: Container(
+            // ios刘海屏的手机显得非常小 50
+            height: Platform.isIOS ? 80 : 50,
+            decoration: BoxDecoration(color: Colors.black),
+            child: TabBar(
+                labelStyle: TextStyle(height: 0, fontSize: 10),
+                // 隐藏指示器
+                indicator: BoxDecoration(color: Color(clearColor)),
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.home),
+                    text: '首页',
+                  ),
+                  Tab(
+                    icon: Icon(Icons.shop),
+                    text: '商城',
+                  ),
+                  Tab(
+                    //Another exception was thrown: No TabController for TabBar.
+                    icon: Icon(Icons.verified_user),
+                    text: '我的',
+                  ),
+                ])),
       ),
     );
   }
